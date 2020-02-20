1a08ce1138d740debab3a9d952c25e8b2128ebb6
geezylucas <sorec@live.com>
2020-02-20 14:21:55 -0600

    Ready lane by lane comparison

--- compare/__main__.py
+++ compare/__main__.py
@@ -10,7 +10,8 @@ path_files = ""
 def open_files():
     global path_files
 
-    path_files = sys.argv[1] + '\\'
+    path_files = "/home/geezylucas/Documentos/Python3/floorfiles/"
+    # path_files = sys.argv[1] + '\\'
 
     list_files = [f for f in os.listdir(path_files)]
 
@@ -24,13 +25,24 @@ def open_files():
 
 
 def calculate(file_two, file_nine):
-    my_data = genfromtxt(path_files + file_two, usecols=(8, 9),
+    my_data = genfromtxt(path_files + file_two, usecols=(4, 5, 6, 8, 9),
                          dtype=None, delimiter=',', skip_header=1)
 
+    list_diff_ocu = []
     count_two = 0
     for row in my_data:
-        if row[0] != 0 and row[1] != 0:
-            count_two += (row[1] - row[0]) + 1
+        if row[3] != 0 and row[4] != 0:
+            count_occurrences = (row[4] - row[3]) + 1
+            count_two += count_occurrences
+
+            text_to_search = f"{row[0]},{row[1]},{(row[2].decode('utf-8'))}"
+            occurrences_found = int(
+                open(path_files + file_nine, 'r').read().count(text_to_search))
+
+            difference = occurrences_found - count_occurrences
+
+            if difference != 0:
+                list_diff_ocu.append(text_to_search + ": " + str(difference))
 
     header_nine = ''
     with open(path_files + file_nine, 'r') as f:
@@ -39,7 +51,7 @@ def calculate(file_two, file_nine):
     result = "Turno " + file_two[-3] + ": " + \
         str(int(header_nine[-6:]) - count_two)
 
-    return result
+    return result, list_diff_ocu
 
 
 if __name__ == '__main__':
@@ -56,16 +68,31 @@ if __name__ == '__main__':
                                   or x.endswith('69A'), files))
 
             if len(files_4):
-                print(calculate(next((f for f in files_4 if f.endswith('2A')), None),
-                                next((f for f in files_4 if f.endswith('9A')), None)))
+                result, list_diff = calculate(next((f for f in files_4 if f.endswith('2A')), None),
+                                              next((f for f in files_4 if f.endswith('9A')), None))
+
+                print(result)
+                if result != 0:
+                    for p in list_diff:
+                        print("\t" + p, end="\n")
 
             if len(files_5):
-                print(calculate(next((f for f in files_5 if f.endswith('2A')), None),
-                                next((f for f in files_5 if f.endswith('9A')), None)))
+                result, list_diff = calculate(next((f for f in files_5 if f.endswith('2A')), None),
+                                              next((f for f in files_5 if f.endswith('9A')), None))
+                print(result)
+                if result != 0:
+                    for p in list_diff:
+                        print("\t" + p, end="\n")
 
             if len(files_6):
-                print(calculate(next((f for f in files_6 if f.endswith('2A')), None),
-                                next((f for f in files_6 if f.endswith('9A')), None)))
+                result, list_diff = calculate(next((f for f in files_6 if f.endswith('2A')), None),
+                                              next((f for f in files_6 if f.endswith('9A')), None))
+
+                print(result)
+                if result != 0:
+                    for p in list_diff:
+                        print("\t" + p, end="\n")
+
     except BaseException:
         import sys
         print(sys.exc_info()[0])
